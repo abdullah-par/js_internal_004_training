6ba70bd49e290a195d2816901667efcb456b3faa
Abdullah Parvez <abdullahparvez43@gmail.com>
2025-12-09 05:23:31 +0000

    "day2-learning-datatypes"

---
+++ day2.js
@@ -0,0 +1,27 @@
+// let a = 23
+// const b = "abc"
+// var c  = 78
+// d = 88
+//displaying the variables
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+//type casting and error printing
+
+//redeclare
+// let a = null
+// let b = ""
+// let c = null
+// let d;
+// console.log("value of a:-" + a)
+// console.log("value of b:-" + b)
+// console.log("value of b:-" + c)
+// console.log("value of b:-" + d)
+// console.log(typeof a)
+let score = "33marks"
+let newVariable = Number(score)
+console.log(typeof score)
+console.log(typeof(newVariable))
+console.log(score)
+console.log(newVariable)
