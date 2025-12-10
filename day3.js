75be03b737734ad53162c7dfea51f80e9de05465
Abdullah Parvez <abdullahparvez43@gmail.com>
2025-12-10 05:18:51 +0000

    Higher-Order-Functions-Practice-Done

--- day3.js
+++ day3.js
@@ -26,3 +26,14 @@ for(let item of fruits){
     currentValue -> current element of array being processed
     initialValue -> starting value for accumulator
 */
+//practice time
+let numbers = [2,4,6,8]
+let doubled = numbers.map(n => n*2)
+console.log(doubled)
+let number = [10,25,30,5,60]
+let greaterValues = number.filter(n=>n>20)
+console.log(greaterValues)
+let num = [1,2,3,4,5]
+acc= 0
+let sum = num.reduce((acc,n)=>acc+n,0)
+console.log(sum)
